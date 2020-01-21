1da54caf0d248e843438304834f8928e376ab984
Eugene2891 <eugene_korban@mail.ru>
2020-01-21 22:14:15 +0300

    Change 3

--- MyNewProject/Controllers/HomeController.cs
+++ MyNewProject/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MyNewProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page2.";
+            ViewBag.Message = "Your application description page3.";
 
             return View();
         }
